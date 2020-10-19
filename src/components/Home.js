9247537330c7d370be1623af046f744937a0e050
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-10-18 23:41:53 -0400

    consuming api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {Component}from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return (
-        <div className='container'>
-            <h4 className='center'>Home</h4>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit vero beatae incidunt. Optio, eos. Dolore vel repellendus itaque numquam provident non distinctio, libero sunt cum perspiciatis dolorem commodi facilis quod. </p>
-        </div>
+class Home extends Component {
 
-    )
+    state = {
+        post: []
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then( response => {
+            this.setState({
+                post: response.data.slice(0,10)
+            })
+            console.log (typeof(this.state.post))
+        })
+    }
+    render () {
+        
+        return (
+            <div className='container'>
+                <h4 className='center'>Home</h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit vero beatae incidunt. Optio, eos. Dolore vel repellendus itaque numquam provident non distinctio, libero sunt cum perspiciatis dolorem commodi facilis quod. </p>
+            </div>
+    
+        )
+    }
+  
 }
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 
 const Navbar = () =>{
     return(
         <nav className="nav wrapper red darken 3">
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo">Poke's time</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+        <div className ="nav-wrapper">
+          <a href="#" className="brand-logo">Poke's time</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </div>
       </nav>
