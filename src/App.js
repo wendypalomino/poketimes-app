ec039489b18e24dac1578e321e1a92dd56862a12
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-10-19 14:43:42 -0400

    adding link to specific pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import {Route, BrowserRouter} from 'react-router-dom';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Post from './components/Post'
 
 class App extends Component {
   render () {
@@ -11,9 +12,12 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Navbar/>
+          <Switch>
           <Route path='/' exact component={Home}/>
           <Route path='/about' component={About}/>
           <Route path='/contact' component={Contact}/>
+          <Route path='/:post_id' component={Post}/>
+          </Switch>
         </div>
       </BrowserRouter>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,48 @@
-import React, {Component}from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
 
     state = {
-        post: []
+        posts: []
     }
-    componentDidMount(){
+    componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then( response => {
-            this.setState({
-                post: response.data.slice(0,10)
+            .then(response => {
+                this.setState({
+                    posts: response.data.slice(0,10)
+                })
             })
-            console.log (typeof(this.state.post))
-        })
     }
-    render () {
-        
+
+
+    render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className='post card' key={post.id}>
+                        <div className='card-content'>
+                            <Link to={'/' + post.id}>
+                            <span className='card-title'>{post.title}</span>
+                            </Link>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (<div>No post yet</div>)
+
         return (
             <div className='container'>
                 <h4 className='center'>Home</h4>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit vero beatae incidunt. Optio, eos. Dolore vel repellendus itaque numquam provident non distinctio, libero sunt cum perspiciatis dolorem commodi facilis quod. </p>
+                {postList}
             </div>
-    
+
         )
     }
-  
+
 }
 
 export default Home;
